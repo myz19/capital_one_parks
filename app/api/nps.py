aaea5df16931c9a9112341ea3458a4ba9a13f5e0
Michelle Zhu <mixue.zhu@gmail.com>
2021-11-07 13:34:08 -0500

    updated menu links

--- app/api/nps.py
+++ app/api/nps.py
@@ -22,9 +22,12 @@ def get_activities():
 def get_parks(activity):
     limit = int(request.args.get('limit'))
     start = int(request.args.get('start'))
-    if session.get("park_data", '') == '':
-        session["park_data"] = get_data('activities/parks', {'id': activity})['data'][0]['parks']
-    number_of_parks = len(session["park_data"])
+    # if session.get("park_data", '') == '':
+    #     session["park_data"] = get_data('activities/parks', {'id': activity})['data'][0]
+    session["park_data"] = get_data('activities/parks', {'id': activity})['data'][0]
+    # return {"message": session["park_data"]}
+    number_of_parks = len(session["park_data"]["parks"])
+    name_of_activity = session["park_data"]["name"]
     end = 0
     hide_next_menu = False
     if (start+limit > number_of_parks):
@@ -34,7 +37,7 @@ def get_parks(activity):
         end = start+limit
     print(f'start is {start} and end is {end}')
     parks_list = []
-    for park in session["park_data"][start:end]:
+    for park in session["park_data"]["parks"][start:end]:
         _data = get_data('parks', {'parkCode': park['parkCode']})['data']
         if len(_data) != 0:
             park_dict = {
@@ -44,9 +47,9 @@ def get_parks(activity):
                 'image': _data[0]['images'][0]['url']
             }
             parks_list.append(park_dict)
-    print(number_of_parks)
+    # print(number_of_parks)
     url = request.path+"?limit=10&start="+str(end)
-    return render_template('listofparks.html', data = parks_list, url = url, hide = hide_next_menu)
+    return render_template('listofparks.html', data = parks_list, url = url, hide = hide_next_menu, name_activity = name_of_activity)
 
 @parks_blueprint.route('/activities/park/<parkcode>')
 def get_park_data(parkcode):
